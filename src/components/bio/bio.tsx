a5e268c621386cdf61a30e1f7d6397bf8deb5337
FrancoBreugem <francobreugem@gmail.com>
2023-11-14 20:29:41 +0200

    Poppins font, pop colour and sizes

--- src/components/bio/bio.tsx
+++ src/components/bio/bio.tsx
@@ -6,15 +6,19 @@ function Bio() {
   return (
     <div className='bio'>
       <h1>
-        Hey, I'm Franco.
+        Hey, I'm Franco<span className='size-pop color-pop'>.</span>
       </h1>
 
+      <h2>
+        I'm a <span className='color-pop'>Full Stack Software Developer</span>
+      </h2>
+
       <p>
-        Hey there! I'm Franco, a full stack web developer hailing from Pretoria, South Africa. From ideas to deployments, I craft robust web applications with a keen focus on front-end frameworks like Angular and ReactJS. Skilled in languages like SQL, C#, Python, and JavaScript, I also handle various tools including Azure DevOps, Visual Studio, SQL Server, and GitHub with comfort.
+        Hey there! I'm Franco, a full stack web developer hailing from Pretoria, South Africa. From ideas to deployments, I craft robust web applications with a keen <span className='color-pop'>focus on front-end frameworks</span> like Angular and ReactJS. Skilled in <span className='color-pop'>languages</span> like SQL, C#, Python, and JavaScript, I also handle various <span className='color-pop'>tools</span> including Azure DevOps, Visual Studio, SQL Server, and GitHub with comfort.
       </p>
 
       <div>
-        <img src={LinkedInIcon} alt="LinkedIn Icon" />
+        <img src={LinkedInIcon} alt="LinkedIn Icon" className="svg-icon" />
       </div>
     </div>
   )
